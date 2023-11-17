da390251affddad3be824fecf14b102ad363ae17
Robert Schmid <r.schmid@outlook.com>
2023-11-17 13:46:32 +0100

    Next: add asset prefix

--- testreport/next.config.js
+++ testreport/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   basePath: '/system-model-validation',
+  assetPrefix: '/system-model-validation/',
   webpack: (config, { defaultLoaders }) => {
     config.module.rules.push({
       test: /\.xml$/,
