bc79dd805860985ba4c39e863ec88f23811a29ec
Robert Schmid <r.schmid@outlook.com>
2026-04-10 17:04:01 +0200

    Set base path

--- utils/testreport/next.config.js
+++ utils/testreport/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.BASE_PATH || '',
   webpack: (config, { defaultLoaders }) => {
     config.module.rules.push({
       test: /\.xml$/,
