8aacb26d3c02c3f0301eb628a691157dd0944bdb
Robert Schmid <r.schmid@outlook.com>
2026-04-10 08:28:46 +0200

    Top-level makefile per baseline

---
+++ EulynxBaseline4Release1/02_Transformation/ClassParsing.cs
@@ -0,0 +1,34 @@
+using XmiToCode.Parsing;
+using XmiToCode.Parsing.Model;
+
+namespace Eulynx.Validation;
+
+[TestClass]
+public class ClassParsing
+{
+    public const string EULYNX_XMI_FILE = "cleaned.xmi";
+    public static IEnumerable<object[]> UmlClasses
+    {
+        get
+        {
+            var processor = new EulynxV19XmiParser(EULYNX_XMI_FILE);
+            foreach (var package in processor.InterestingPackages) {
+                var classes = Package.ClassElements(package);
+                foreach (var (Element, _) in classes)
+                    yield return new object[] { package.Name, Element.Name };
+            }
+        }
+    }
+
+    [TestMethod, TestCategory("parse-classes")]
+    [DynamicData(nameof(UmlClasses))]
+    public void ParseClass(string package, string className)
+    {
+        var processor = new EulynxV19XmiParser(EULYNX_XMI_FILE);
+        var umlPackage = processor.InterestingPackages.Single(x => x.Name == package);
+        var pkg = Package.CreateFromUml(umlPackage, processor.GlobalContext);
+        var (Element, Hierarchy) = pkg.ClassElements().Single(x => x.Element.Name == className);
+        // Assert that this doesn't throw
+        Package.ParseClass(Element, pkg.Context, Hierarchy);
+    }
+}
---
+++ EulynxBaseline4Release1/02_Transformation/Compilation.cs
@@ -0,0 +1,83 @@
+using System.Diagnostics;
+using XmiToCode.Identifiers;
+using XmiToCode.Parsing;
+using XmiToCode.Parsing.Model;
+
+namespace Eulynx.Validation;
+
+[TestClass]
+public class Compilation
+{
+    public static IEnumerable<object[]> UmlClasses
+    {
+        get
+        {
+            var processor = new EulynxV19XmiParser(ClassParsing.EULYNX_XMI_FILE);
+            foreach (var package in processor.InterestingPackages) {
+                var classes = Package.ClassElements(package);
+                foreach (var (Element, _) in classes)
+                    yield return new object[] { package.Name, Element.Name };
+            }
+        }
+    }
+
+    [TestMethod, TestCategory("compile-c")]
+    [DynamicData(nameof(UmlClasses))]
+    public void CompileC(string package, string className)
+    {
+        using var process = new Process();
+
+        var output = Environment.GetEnvironmentVariable("CODEGEN_OUTPUT_DIR") ?? throw new Exception("CODEGEN_OUTPUT_DIR not set");
+        var workingDir = $"{output}/{new TypeIdentifier(package).Name}";
+        var inFile = $"{new TypeIdentifier(className).Name}.c";
+        var outFile = $"{new TypeIdentifier(className).Name}.o";
+
+        var info = new FileInfo($"{workingDir}/{inFile}");
+        if (!info.Exists)
+            Assert.Inconclusive();
+
+        process.StartInfo.FileName = "make";
+        process.StartInfo.Arguments = $"-C {workingDir} {outFile}";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.EnvironmentVariables["CFLAGS"] = "-Werror";
+        process.Start();
+
+        Console.WriteLine(process.StandardOutput.ReadToEnd());
+        Console.WriteLine(process.StandardError.ReadToEnd());
+
+        process.WaitForExit();
+        Assert.AreEqual(0, process.ExitCode);
+    }
+
+    [TestMethod, TestCategory("compile-klee")]
+    [DynamicData(nameof(UmlClasses))]
+    public void CompileKlee(string package, string className)
+    {
+        using var process = new Process();
+
+        var output = Environment.GetEnvironmentVariable("CODEGEN_OUTPUT_DIR") ?? throw new Exception("CODEGEN_OUTPUT_DIR not set");
+        var workingDir = $"{output}/{new TypeIdentifier(package).Name}";
+        var inFile = $"{new TypeIdentifier(className).Name}.c";
+        var outFile = $"{new TypeIdentifier(className).Name}.bc";
+
+        var info = new FileInfo($"{workingDir}/{inFile}");
+        if (!info.Exists)
+            Assert.Inconclusive();
+
+        process.StartInfo.FileName = "make";
+        process.StartInfo.Arguments = $"-f {Environment.CurrentDirectory}/../../../../Klee/Makefile -C {workingDir} {outFile}";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.EnvironmentVariables["CFLAGS"] = "-Werror";
+        process.Start();
+
+        Console.WriteLine(process.StandardOutput.ReadToEnd());
+        Console.WriteLine(process.StandardError.ReadToEnd());
+
+        process.WaitForExit();
+        Assert.AreEqual(0, process.ExitCode);
+    }
+}
